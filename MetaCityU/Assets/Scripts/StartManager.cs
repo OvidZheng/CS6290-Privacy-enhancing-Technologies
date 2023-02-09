2cf39e794c058f251418230135cf596ed246d57c
OvidZ <zdianxin@outlook.com>
2023-02-09 22:35:06 +0800

    Feat: 成功获取余额信息

--- MetaCityU/Assets/Scripts/StartManager.cs
+++ MetaCityU/Assets/Scripts/StartManager.cs
@@ -1,5 +1,7 @@
+using System.Threading.Tasks;
 using UnityEngine;
 using Thirdweb;
+
 
 public class StartManager : MonoBehaviour
 {
@@ -9,6 +11,8 @@ public class StartManager : MonoBehaviour
     [SerializeField] private GameObject connectedButton;
 
     [SerializeField] private TMPro.TextMeshProUGUI addressTxt;
+
+    [SerializeField] private TMPro.TextMeshProUGUI balanceTxt;
 
     public async void ConnectToWallet()
     {
@@ -22,5 +26,15 @@ public class StartManager : MonoBehaviour
         addressTxt.text = address;
         connectButton.SetActive(false);
         connectedButton.SetActive(true);
+
+        await CheckBalance();
+    }
+
+    public async Task CheckBalance()
+    {
+        balanceTxt.text = "Start Fetching Balance";
+        Contract contract = SDKManager.Instance.SDK.GetContract("0xcF157113bD328Ab070DBb1eD74747225a7BDD67d");
+        CurrencyValue balance = await contract.ERC20.Balance();
+        balanceTxt.text = balance.displayValue;
     }
 }
